d17b7fc0b3fd8dd694d91abbc15ca8e19137e8a9
Yogesh Singh <ayogesh1214@gmail.com>
2026-02-18 11:56:49 +0530

    remove duplicates 2

--- lc/removeduplicates.js
+++ lc/removeduplicates.js
@@ -1,69 +1,31 @@
 /**
-Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
-
-Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-
-Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
-Return k.
-Custom Judge:
-
-The judge will test your solution with the following code:
-
-int[] nums = [...]; // Input array
-int val = ...; // Value to remove
-int[] expectedNums = [...]; // The expected answer with correct length.
-                            // It is sorted with no values equaling val.
-
-int k = removeElement(nums, val); // Calls your implementation
-
-assert k == expectedNums.length;
-sort(nums, 0, k); // Sort the first k elements of nums
-for (int i = 0; i < actualLength; i++) {
-    assert nums[i] == expectedNums[i];
-}
-If all assertions pass, then your solution will be accepted.
-
- 
-
 Example 1:
 
-Input: nums = [3,2,2,3], val = 3
-Output: 2, nums = [2,2,_,_]
-Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 Example 2:
 
-Input: nums = [0,1,2,2,3,0,4,2], val = 2
-Output: 5, nums = [0,1,4,0,3,_,_,_]
-Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
-Note that the five elements can be returned in any order.
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
  */
 
-var removeElement = function (nums, val) {
-  let slow = nums.length - 1;
-  while (nums[slow] === val) {
-    slow--;
-  }
-  console.log(slow);
-  if (slow < 0) {
-    return [];
-  }
-  if (slow < 0) return [];
-  for (let i = slow - 1; i >= 0; i--) {
-    if (nums[i] === val) {
-      slow--;
-      [nums[slow], nums[i]] = [nums[i], nums[slow]];
+var removeDuplicates = function (nums) {
+  if (nums.length <= 2) return nums.length;
+
+  let slow = 2;
+
+  for (let fast = 2; fast < nums.length; fast++) {
+    if (nums[fast] !== nums[slow - 2]) {
+      nums[slow] = nums[fast];
+      slow++;
     }
   }
-  console.log(slow);
-  return slow === 0 ? [] : slow;
-
-  console.log(nums, slow);
+  console.log(nums);
+  return slow;
 };
-
-// removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
-// removeElement([3, 2, 2, 3], 3);
-// removeElement([3, 3], 3);
-console.log(removeElement([2], 3));
-//console.log(removeElement([3, 3], 3));
+//  nc= 2                           s 
+console.log(removeDuplicates([1,1,2,2,3,3]));
