94c0a802aaa15aeb61354414af1c94d8e574b429
Yogesh Singh <ayogesh1214@gmail.com>
2026-02-26 10:50:01 +0530

    remove nth node

---
+++ lc/revision1.js
@@ -0,0 +1,25 @@
+
+/**
+Squares of a Sorted Array
+Patten: 
+3Sum Smaller
+Linked List Cycle
+Sort Colors
+Container With Most Water
+Happy Number
+Remove Duplicates from Sorted Array II
+Boats to Save People
+Intersection of Two Arrays
+Delete the Middle Node of a Linked List
+Two Sum II - Input Array Is Sorted
+Move Zeroes
+4Sum
+Remove Element
+3Sum Closest
+Find the Duplicate Number
+String Compression
+Middle of the Linked List
+Remove Duplicates from Sorted Array
+Is Subsequence
+3Sum
+ */
