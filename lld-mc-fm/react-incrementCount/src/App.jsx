f70a54853cc8da63aae68009ba294956d7ac5d98
Yogesh Singh <ayogesh1214@gmail.com>
2026-02-26 15:48:11 +0530

    approach two of incre count

--- lld-mc-fm/react-incrementCount/src/App.jsx
+++ lld-mc-fm/react-incrementCount/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
-/**1-- this is what problem asked and this is a simple solution to do it , storing interval id in useRef, clearing the interval on stopbutton */
 function App() {
   const [count, setCount] = useState(0);
   let intervalRef = useRef(null);
@@ -9,6 +8,21 @@ function App() {
   function startButton() {
     intervalRef.current = setInterval(() => {
       setCount(prev => prev + 1);
+      /**1--
+       * 
+       * same here
+       * function handleClick() {
+  fetch("https://dummyjson.com/test")
+    .then(() => {
+      setCount(count + 1);
+    });
+}
+    count here will be the value when handleclick was made, multiple handleclick will do just setCount(0+1)
+
+  so in javascript  function remembers the variables as they were when the function was created, not when it runs later.
+  hence doing setCount(prev=>prev+1) gives the latest state value
+       * 
+       */
     }, 1000);
   }
 
@@ -20,9 +34,42 @@ function App() {
     <div className="box">
       <button onClick={startButton}>{"start"}</button>
       {count}
-      <button onClick={stopButton}>{'stop'}</button>
+      <button onClick={stopButton}>{"stop"}</button>
     </div>
   );
 }
 
-export default App;
+function AppUseEffect() {
+  /**2-- second way of doing it , first count then countClean */
+  const [count, setCount] = useState(0);
+  const [start, setStart] = useState(false);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    if (start) {
+      console.log(count, "count");
+      timerRef.current = setTimeout(() => {
+        setCount(count + 1);
+      }, 1000);
+    }
+    return () => {
+      console.log(count, "countClean");
+      clearTimeout(timerRef.current);
+    };
+  }, [count, start]);
+  function startButton() {
+    setStart(true);
+  }
+  function stopButton() {
+    clearTimeout(timerRef.current);
+    setStart(false);
+  }
+  return (
+    <div className="box">
+      <button onClick={startButton}>{"start"}</button>
+      {count}
+      <button onClick={stopButton}>{"stop"}</button>
+    </div>
+  );
+}
+export default AppUseEffect;
