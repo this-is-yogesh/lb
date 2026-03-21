05f730c737d3256145da02639d82e202934c329f
Yogesh Singh <ayogesh1214@gmail.com>
2026-03-21 12:50:39 +0530

    wrappig up with css generator selector

---
+++ lld-mc-fm/generate_css_selector_js/index.js
@@ -0,0 +1,17 @@
+let root = document.getElementById("root");
+let target = document.getElementById("target");
+
+function cssGenerateSelector(root, target) {
+  const selectors = new Array();
+  while (root !== target) {
+    let nth = Array.from(target.parentNode.children).indexOf(target) + 1;
+    let selector = `${target.tagName.toLowerCase()}:nth-child(${nth})`;
+    selectors.unshift(selector);
+    target = target.parentNode;
+  }
+  selectors.unshift(`#${target.id}`);
+
+  return selectors.join(">");
+}
+
+console.log(cssGenerateSelector(root, target));
